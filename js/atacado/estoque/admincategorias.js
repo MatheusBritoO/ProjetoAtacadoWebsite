3f739f3a90f68c2313146b60ad0cec48eea70229
Matheus Brito <matheusovelar77@gmail.com>
2022-07-22 17:25:29 -0400

    Melhorando Categoria

--- js/atacado/estoque/admincategorias.js
+++ js/atacado/estoque/admincategorias.js
@@ -1,107 +1,159 @@
-$(document).ready(function(){
-   AvaliarOperacao();
 
-   $('#btnCANCELAR').click(function() {
-    localStorage.removeItem('opercat');
-    if ((operacao == 2) || (operacao == 3)){
-        var operacao = localStorage.removeItem('opercatid')
-    }
-    window.location = 'categorias.html';
-   });
-}); 
-
-function AvaliarOperacao(){
-    var operacao = localStorage.getItem('opercat')
-    if (operacao == 1){
-        $('#lblOPERACAO').text('INCLUSAO');
-    }
-    else if (operacao == 2){
-        $('#lblOPERACAO').text('ALTERAÇÃO');
-        var id = localStorage.getItem('opercatid')
-        CarregarCategoria(id);
-    }
-    else if (operacao == 3){
-        $('#lblOPERACAO').text('EXCLUSÃO');
-        var id = localStorage.getItem('opercatid')
-        CarregarCategoria(id);
-        PrepararExclusao();
-    }
-    else
-    {
-        alert('operação Inválida!!!');
-    }
-}
-
-function CarregarCategoria(id){
-    var urlServico = 'https://localhost:7281/api/Categoria/' + id;
-    $.get(urlServico, function(retorno, status){
-        if(retorno == ''){
-            alert('Categoria não existe ('+ id +')!!!')
-            return;
-        }
-        else{
-            $('#txtID').val(retorno.codigo);
-            $('#txtID').prop('readonly', true);
-            
-            $('#txtDESCRICAO').val(retorno.descricao);
-
-            if (retorno.situacao == true){
-                $('#radTRUE').prop('checked', true);
-            }
-            else{
-                $('#radFALSE').prop('checked', true);
-            }
-        }
-    });
-}
-
-    function PrepararExclusao() {
-        $('#txtID').prop('readonly', true);
-        $('#txtDESCRICAO').prop('readonly', true);
-        $('#radTRUE').prop('disabled', true);
-        $('#radFALSE').prop('disabled', true);
-    }
-
-    function Confirmar(){
-        var operacao = localStorage.getItem('opercat')
-        if (operacao == 1){
-            AcionarInclusao();
-        }
-        else if (operacao == 2){
-
-        }
-        else if (operacao == 3){
-
-        }
-    }
-    
+$(document).ready( function(){ 
+    AvaliarOperacao();
  
-    function AcionarInclusao(){
+    $('#btnCANCELAR').click(function(){
+        Cancelar();
+    });
+ });
+ 
+
+ function Cancelar(){
+ var operacao = localStorage.getItem('opercat');
+     if ((operacao == 2) || (operacao == 3)){
+         localStorage.removeItem('opercatid');
+     }
+         localStorage.removeItem('opercat');
+         window.location = 'categorias.html'
+}
+
+ function AvaliarOperacao(){
+     var operacao = localStorage.getItem('opercat');
+     if(operacao == 1){
+         $('#lblOPERACAO').text('INCLUSÃO');
+     }
+     else if(operacao == 2){
+         $('#lblOPERACAO').text('ALTERAÇÃO');
+         var id = localStorage.getItem('opercatid');
+         CarregarCategoria(id);
+     }
+     else if(operacao == 3){
+         $('#lblOPERACAO').text('EXCLUSÃO');
+         var id = localStorage.getItem('opercatid');
+         CarregarCategoria(id);
+         PrepararExclusao();
+     }
+     else {
+         alert('Operaçãoa Inválida!!!');
+     }
+ 
+ }
+ 
+ function CarregarCategoria(id) {
+     var urlServico = 'https://localhost:7281/api/Categoria/'+ id;
+     $.get(urlServico, function(retorno,status){
+         if (retorno == ''){
+             alert('Categoria não existe(' + id + ')!!!');
+         }
+         else{
+             $('#txtID').val(retorno.codigo);
+             $('#txtID').prop('readonly',true);
+             $('#txtID').prop('disabled',true);
+
+             $('#txtDESCRICAO').val(retorno.descricao);
+             
+             if (retorno.situacao == true){
+                 $('#radTRUE').prop('checked',true); 
+             }
+             else{
+                 $('#radFALSE').prop('checked',true); 
+             }
+         }
+ 
+     });
+ 
+ }
+ 
+ function PrepararExclusao(){
+     $('#txtID').prop('readonly',true);
+     $('#txtID').prop('disabled',true);
+
+     $('#txtDESCRICAO').prop('readonly',true);
+     $('#txtDESCRICAO').prop('disabled',true);
+
+     $('#radTRUE').prop('disabled',true);
+     $('#radFALSE').prop('disabled',true);
+ }
+ 
+    
+ function Confirmar(){
+     var operacao = localStorage.getItem('opercat');
+     if(operacao == 1){
+         AcionarInclusao();
+     }
+     else if(operacao == 2){
+        AcionarAlteracao();
+     }
+     else if(operacao == 3){    
+        AcionarExclusao();
+     }
+     else {
+         alert('Operaçãoa Inválida!!!');
+     } 
+ }
+ 
+ function AcionarInclusao() {
+     var categoria = {
+         codigo: 0,
+         descricao: "string",
+         situacao: true
+     };
+    
+     categoria.descricao = $('#txtDESCRICAO').val();     
+     categoria.situacao = $("input[name='situacao']:checked").is(':checked') ? true : false;
+     var urlServico = 'https://localhost:7281/api/Categoria';
+    
+     $.ajax({
+        url : urlServico,
+        type: 'post',
+        dataType: 'json',
+        data: JSON.stringify(categoria),
+        contentType: 'application/json',
+        success: function(data){
+            alert('Categoria salva com sucesse (ID:' + data.codigo +').');
+            Cancelar();
+        } 
+     });       
+     }
+    
+     function AcionarAlteracao(){
         var categoria = {
             codigo: 0,
             descricao: "string",
             situacao: true
         };
-        categoria.descricao = $('#txtDESCRICAO').val();
-        if ($('#radTRUE').val()== true){
-            categoria.situacao = true;
-        }
-        else{
-            categoria.situacao = false;
-        }
-        var urlServico = 'https://localhost:7281/api/Categoria/';
-        $.post(urlServico, categoria, function(retorno, situacao){
-            if (retorno == ""){
-                alert('Ocorreu um erro ao executar a inclusão.');
-            }
-            else{
-              if  (retorno.codigo != 0){
-                alert('Inclusão realizada com sucesso.(ID: '+ retorno.codigo+').');
-              }
-                
-            }
+       categoria.codigo = $('#txtID').val();
+        categoria.descricao = $('#txtDESCRICAO').val();     
+        categoria.situacao = $("#radTRUE").is(':checked') ? true : false;
 
-        });
-    }
+        var urlServico = 'https://localhost:7281/api/Categoria';
+       
+        $.ajax({
+           url : urlServico,
+           type: 'put',
+           dataType: 'json',
+           data: JSON.stringify(categoria),
+           contentType: 'application/json',
+           success: function(data){
+               alert('Categoria alterada com sucesse (ID:' + data.codigo +').');
+               Cancelar();
+           } 
+        });       
+     }
 
-  
+     function AcionarExclusao(){
+        var id = localStorage.getItem('opercatid');
+         var urlServico = 'https://localhost:7281/api/Categoria/' + id;
+        
+         $.ajax({
+            url : urlServico,
+            type: 'delete',
+            dataType: null,
+            data: null,
+            contentType: 'application/json',
+            success: function(data){
+                alert('Categoria excluida com sucesso (ID:' + data.codigo +').');
+                Cancelar();
+            } 
+         });       
+     }
--- js/atacado/estoque/produto.js
+++ js/atacado/estoque/produto.js
@@ -2,81 +2,85 @@ $(document).ready( function(){
     CarregarCategorias();
 
     $('#ddlCAT').change(function() {
-        var idcat = $('#ddlCAT option:selected').val();
-        CarregarSubcategorias(idcat);
+        $('#tblPROT tbody').empty();
+        var subid = $('#ddlCAT option:selected').val();
+        CarregarSubcategoria(subid);
     });
-
     $('#ddlSUB').change(function() {
-        var idsub = $('#ddlSUB option:selected').val();
-        CarregarProdutos(idsub);
+        var protid = $('#ddlSUB option:selected').val();
+        CarregarProduto(protid);
     });
-
 });
-
-function CarregarCategorias() {
-    var urlServico = 'https://localhost:7281/api/Categoria';
-    $.get(urlServico, function (retorno, status) {
-        var keys = Object.keys(retorno);
-        if (keys.lenght == 0) {
-            alert("Erro ao obter os dados.");
-        }
-        else {
-            for (var i = 0; i < retorno.length; i++) {
-                var categoria = retorno[i];
-                var id = categoria.codigo;
-                var descricao = categoria.descricao;
-                var opcao = '<option value="' + id + '">' + descricao + '</option>';
-                $('#ddlCAT').append(opcao);
+    
+     function CarregarCategorias() {
+        var urlServico = 'https://localhost:7281/api/Categoria';
+        $.get(urlServico, function (retorno, status) {
+            if (retorno.lenght == 0) {
+                alert("Erro ao obter os dados.");
             }
-        }
-    });
-}
-
-function CarregarSubcategorias(idcat) {
-    var urlServico = 'https://localhost:7281/api/Subcategoria/PorCategoria/' + idcat;
-    $.get(urlServico, function (retorno, status) {
-        var keys = Object.keys(retorno);
-        if (keys.lenght == 0) {
-            alert("Erro ao obter os dados.");
-        }
-        else{
-            for (var i = 0; i <retorno.length; i++){
-                var subcategoria = retorno[i];
-                var idSubcategoria = subcategoria.idSubcategoria;
-                var descricao = subcategoria.descricaoSubcategoria;
-                var opcao = '<option value="' + idSubcategoria+ '">' + descricao + '</option>';
-                $('#ddlSUB').append(opcao);
-            }
-        }
-    });
-}
-function CarregarProdutos(idsub) {
-    var urlServico = 'https://localhost:7281/api/Produto/PorSubcategoria/' + idsub;
-    $.get(urlServico, function (retorno, status) {
-        var keys = Object.keys(retorno);
-        if (keys.lenght == 0) {
-            alert("Erro ao obter os dados.");
-        }
-        else{
-            for (var i = 0; i <retorno.length; i++){
-                var  produto = retorno[i];
-                var idProduto = produto.idSubcategoria;
-                var idSubcategoria = produto.descricaoSubcategoria;
-                var idCategoria = produto.idCategoria;
-                var Descricao = produto.descricaoProduto;
+            else {
+               for (var i = 0; i <retorno.length; i++) {
+                    var categoria = retorno[i];
+                    var idCategoria = categoria.codigo;
+                    var descricao = categoria.descricao;
+                    var opcao = '<option value="' + idCategoria + '">'+ descricao +'</option>';
+                        $('#ddlCAT').append(opcao);
                 
-
-                var linhaINI = "<tr>";
-                var colunaCODIGO = "<td>" + idProduto + "</td>";   
-                var colunaSUB = "<td>" + idSubcategoria + "</td>";
-                var colunaCAT = "<td>" + idCategoria + "</td>"; 
-                var colunaDESCRICAO = "<td>" + descricaoProduto + "</td>";
-                var linhaFIM = "</tr>";
-
-                var linha = linhaINI + colunaCODIGO + colunaSUB + colunaCAT  +colunaDESCRICAO  + linhaFIM;
-
-                $("#tblPRO tbody").append(linha);
+                }
             }
-        }
-    });    
+        });
 }
+
+function CarregarSubcategoria(idsub) {
+        var urlServico = 'https://localhost:7281/api/Subcategoria/PorCategoria/'+ idsub ;
+
+        $.get(urlServico, function(retorno,status) {
+            var keys = Object.keys(retorno);
+            if (keys.lenght == 0) {
+            alert("Erro ao obter os dados.");
+            }
+            else{
+                for (var i = 0; i <retorno.length; i++){
+                    var subcategoria = retorno[i];
+                    var idSubcategoria = subcategoria.idSubcategoria;
+                    var idCategoria = subcategoria.idCategoria;
+                    var descricao = subcategoria.descricaoSubcategoria;
+                    var situacao = subcategoria.situacao; 
+                    var opcao = '<option value="' + idSubcategoria + '">'+ descricao +'</option>';
+                    $('#ddlSUB').append(opcao);
+
+                }
+            }
+        });
+    }
+    function CarregarProduto(idprot) {
+        var urlServico = 'https://localhost:7281/api/Produto/PorSubcategoria/'+ idprot ;
+
+        $.get(urlServico, function(retorno,status) {
+            var keys = Object.keys(retorno);
+            if (keys.lenght == 0) {
+            alert("Erro ao obter os dados.");
+            }
+            else{
+                for (var i = 0; i <retorno.length; i++){
+                    var produto = retorno[i];
+                    var idProduto = produto.idProduto;
+                    var idSubcategoria = produto.idSubcategoria;
+                    var idCategoria = produto.idCategoria;
+                    var descricao = produto.descricaoProduto;
+
+                    var linhaINI = "<tr>";
+                    var colunaPROT =  "<td>"+ idProduto +"</td>";
+                    var colunaIDSUB = "<td>"+ idSubcategoria +"</td>";
+                    var colunaIDCAT = "<td>"+ idCategoria +"</td>";
+                    var colunaDESCRICAO = "<td>"+ descricao +"</td>";
+                    var linhaFIM = "</tr>";
+
+                    var linha = linhaINI + colunaPROT + colunaIDCAT + colunaIDSUB + colunaDESCRICAO + linhaFIM;
+
+                    $("#tblPROT tbody").append(linha);
+                }
+            }
+        });
+    }
+
